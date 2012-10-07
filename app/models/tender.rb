ff14b43ca2d3f97fab965b5777145623e5df6bc3
Jason <jason.addie@gmail.com>
2012-10-07 11:45:53 +0300

    added some aggregate queries; created cpv_name attr; working on scraper code

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -21,4 +21,52 @@ class Tender < ActiveRecord::Base
 
   validates :url_id, :procurring_entity_id, :tender_type, :tender_registration_number, :tender_status, :presence => true
 
+  attr_accessor :sum_estimated_value, :count_tender_status
+  
+  ####################
+  ### special attributes
+  ####################
+  def cpv_name
+    return self.cpv_code[self.cpv_code.index("-")+1, self.cpv_code.length]
+  end
+
+  ####################
+  ### aggregate queries
+  ####################
+  # get top estimated values by cpv code
+  def self.top_cpv_estimated_values(limit)
+    sql = "select cpv_code, sum(estimated_value) as sum_estimated_value "
+    sql << "from tenders "
+    sql << "group by cpv_code "
+    sql << "order by sum_estimated_value desc "
+    sql << "limit :limit " if limit
+    
+    find_by_sql(sql, :limit => limit)
+  end
+
+  # get proportion of tender status 
+  def self.tender_status_proportional
+    values = []
+    sql = "select tender_status, count(*) as count_tender_status "
+    sql << "from tenders "
+    sql << "group by tender_status "
+    sql << "order by count_tender_status desc, tender_status asc "
+    
+    query = find_by_sql(sql)
+    
+    if query && !query.empty?
+      total = query.map{|x| x.count_tender_status}.inject{|sum,x| sum + x }
+      
+      query.each do |item|
+        hash = Hash.new
+        hash[:tender_status] = item.tender_status
+        hash[:number] = item.count_tender_status
+        hash[:percent] = total.to_f / item.count_tender_status * 100
+        values << hash
+      end
+    end
+    return values
+  end
+  
+
 end
--- lib/scraper_file.rb
+++ lib/scraper_file.rb
@@ -10,7 +10,32 @@ module ScraperFile
       puts " - found json file"
       json = JSON.parse(file_data)
       
-      
+      Tender.transaction do
+        json.each do |item|
+          tender = Tender.new
+
+          # basic tender info
+          tender.url_id = item["urlID"]
+          tender.tender_type = item["tenderType"]
+          tender.tender_registration_number = item["tenderRegistrationNumber"]
+          tender.tender_status = item["tenderStatus"]
+          tender.tender_announcement_date = item["tenderAnnouncementDate"]
+          tender.bid_start_date = item["bidStartDate"]
+          tender.bid_end_date = item["bidEndDate"]
+          tender.estimated_value = item["estimatedValue"]
+          tender.cpv_code = item["cpvCode"]
+        
+          # if the organization does not exist yet, add it
+        
+
+          if tender.valid?
+            tender.save
+          else
+            Rails.logger.debug "***** tender was not valid: #{tender.errors} *****"
+		        raise ActiveRecord::Rollback
+          end
+        end
+      end
       
     end
     
