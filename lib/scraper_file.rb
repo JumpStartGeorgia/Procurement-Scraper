8b11693ddbb2c9d1d6858d7b03cc584afd56ef3a
Jason <jason.addie@gmail.com>
2012-10-07 13:59:43 +0300

    added ability to process processing entity json

--- lib/scraper_file.rb
+++ lib/scraper_file.rb
@@ -1,22 +1,30 @@
 module ScraperFile
 
+  FILE_TENDER = "tenders.json"
+  FILE_PROC_ENTITIES = "procuringEntities.json"
+
   def self.process
     start = Time.now
     
     I18n.locale = :en # do this so formating of floats and dates is correct when reading in json
-    # get the json file
-    file_path = "#{Rails.root}/public/system/items.json"    
-    file_data = File.open(file_path, "r") {|f| f.read()}
+    # get the json files
+    tender_file_path = "#{Rails.root}/public/system/#{FILE_TENDER}"    
+    tender_file_data = File.open(tender_file_path, "r") {|f| f.read()}
+    proc_ent_file_path = "#{Rails.root}/public/system/#{FILE_PROC_ENTITIES}"    
+    proc_ent_file_data = File.open(proc_ent_file_path, "r") {|f| f.read()}
     
-    if file_data
-      Rails.logger.debug " - found json file"
+    if tender_file_data && proc_ent_file_data
+      Rails.logger.debug " - found json files"
       # take out extra ',' at end of data
-      json = JSON.parse(file_data.gsub("},]", "}]"))
+      json_tender = JSON.parse(tender_file_data.gsub("},]", "}]"))
+      json_proc_ent = JSON.parse(proc_ent_file_data.gsub("},]", "}]"))
       
       Tender.transaction do
+        # remove old entries
         Tender.delete_all
+        Organization.delete_all
         
-        json.each_with_index do |item, index|
+        json_tender.each_with_index do |item, index|
           tender = Tender.new
 
           # basic tender info
@@ -30,9 +38,25 @@ module ScraperFile
           tender.estimated_value = item["estimatedValue"].gsub("`","").to_f
           tender.cpv_code = item["cpvCode"]
         
-          # if the organization does not exist yet, add it
-          tender.procurring_entity_id = item["procuringEntityID"]
-          tender.procurring_entity_name = item["procuringEntityName"]
+          # find the procurer json record
+          procurrer = json_proc_ent.select{|x| x["urlID"] == item["procuringEntityID"]}
+          if procurrer && !procurrer.empty?
+            # if the procurer does not exist in organization yet, add it
+            organization = Organization.find_by_url_id(procurrer.first["urlID"])
+            if !organization
+              organization = Organization.create(
+                :url_id => procurrer.first["urlID"],
+                :label_id => procurrer.first["OrgID"],
+                :name => procurrer.first["Name"],
+                :country => procurrer.first["Country"]
+              )
+            end
+            tender.procurring_entity_id = organization.id
+          else
+            Rails.logger.debug "***** procuring entity was not found for this tender record #{index} *****"
+		        raise ActiveRecord::Rollback
+		        return "ERROR: Procuring Entity record for tender json record #{index} could not be found in the procuring entity json file."
+          end
 
           if tender.valid?
             tender.save
@@ -47,7 +71,7 @@ module ScraperFile
     end
     
     Rails.logger.debug "###### total time = #{Time.now - start} seconds"
-    return "#{json.length} Tenders were saved in #{Time.now - start} seconds"
+    return "#{json_tender.length} Tenders were saved in #{Time.now - start} seconds"
   end
 
 end
