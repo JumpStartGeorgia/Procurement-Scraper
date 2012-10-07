e2070c3e799c40e3e60e48136e2479b214fef695
Eric Barrett <ericnbarrett@gmail.com>
2012-10-07 14:18:19 +0400

    styled front page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,8 +4,8 @@ class RootController < ApplicationController
   def index
     @recent_tenders = Tender.recent
 #    @recent_bids = Bid.recent.includes()
-    @tender_pie = Tender.tender_status_proportional
-    gon.tender_pie_chart = @tender_pie
+#    @tender_pie = Tender.tender_status_proportional
+#    gon.tender_pie_chart = @tender_pie
   end
 
   def process_json
