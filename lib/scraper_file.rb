396ce3d105e6ab67fe4df1430dfb9aef8758ea8f
Jason <jason.addie@gmail.com>
2012-10-09 13:28:48 +0400

    changed scraper code to show list of tenders with no procurring entity; added formtastic ie support

--- lib/scraper_file.rb
+++ lib/scraper_file.rb
@@ -9,22 +9,22 @@ module ScraperFile
     msgs = []
     I18n.locale = :en # do this so formating of floats and dates is correct when reading in json
     # get the json files
-    tender_file_path = "#{Rails.root}/public/system/#{FILE_TENDER}"    
+    tender_file_path = "#{Rails.root}/public/system/#{FILE_TENDER}"
     tender_file_data = File.open(tender_file_path, "r") {|f| f.read()}
-    proc_ent_file_path = "#{Rails.root}/public/system/#{FILE_PROC_ENTITIES}"    
+    proc_ent_file_path = "#{Rails.root}/public/system/#{FILE_PROC_ENTITIES}"
     proc_ent_file_data = File.open(proc_ent_file_path, "r") {|f| f.read()}
-    
+
     if tender_file_data && proc_ent_file_data
       Rails.logger.debug " - found json files"
       # take out extra ',' at end of data
       json_tender = JSON.parse(tender_file_data.gsub("},]", "}]"))
       json_proc_ent = JSON.parse(proc_ent_file_data.gsub("},]", "}]"))
-      
+
       Tender.transaction do
         # remove old entries
         Tender.delete_all
         Organization.delete_all
-        
+
         json_tender.each_with_index do |item, index|
           tender = Tender.new
 
@@ -38,7 +38,7 @@ module ScraperFile
           tender.bid_end_date = item["bidsEndDate"]
           tender.estimated_value = item["estimatedValue"].gsub("`","").to_f
           tender.cpv_code = item["cpvCode"]
-        
+
           # find the procurer json record
           procurrer = json_proc_ent.select{|x| x["urlID"] == item["procuringEntityID"]}
           if procurrer && !procurrer.empty?
@@ -55,7 +55,7 @@ module ScraperFile
             tender.procurring_entity_id = organization.id
           else
             Rails.logger.debug "***** procuring entity was not found for this tender record #{index} *****"
-		        msgs << "ERROR: Procuring Entity record for tender json record #{index} could not be found in the procuring entity json file."
+		        msgs << tender.url_id
 #		        raise ActiveRecord::Rollback
 #		        break
           end
@@ -70,13 +70,13 @@ module ScraperFile
           end
         end
       end
-      
+
     end
-    
+
     Rails.logger.debug "###### total time = #{Time.now - start} seconds"
     msg = "#{json_tender.length} Tenders were saved in #{Time.now - start} seconds" if msg.nil?
     Rails.logger.debug "msg = #{msg}"
-    Rails.logger.debug "msgs = #{msgs}"
+    Rails.logger.debug "tenders with unfound proc entities = #{msgs}"
     return msg
   end
 
